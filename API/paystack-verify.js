f143e361707614fdb640a7d68d7263d14b42ebdc
nifelux <globalpaysure@gmail.com>
2026-02-23 21:26:08 +0100

    Create paystack-verify.js

---
+++ API/paystack-verify.js
@@ -0,0 +1,9 @@
+export default async function handler(req,res){
+  const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
+  const ref = req.query.ref;
+  const response = await fetch(`https://api.paystack.co/transaction/verify/${ref}`,{
+    headers:{Authorization:`Bearer ${PAYSTACK_SECRET_KEY}`}
+  });
+  const data = await response.json();
+  res.status(200).json(data);
+    }
