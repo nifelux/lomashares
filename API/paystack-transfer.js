6a667dbed7b66f14755daefdce92338fa7569ffd
nifelux <globalpaysure@gmail.com>
2026-02-23 21:48:55 +0100

    Update paystack-transfer.js

--- API/paystack-transfer.js
+++ API/paystack-transfer.js
@@ -1,9 +1,9 @@
 export default async function handler(req,res){
-  const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
+  const sk_live_e2b31fe28d46c220f6059404e44dd875feb854d3 = process.env.sk_live_e2b31fe28d46c220f6059404e44dd875feb854d3;
   const body = req.body; // {email, amount, reason}
   const response = await fetch('https://api.paystack.co/transfer', {
     method: 'POST',
-    headers:{Authorization:`Bearer ${PAYSTACK_SECRET_KEY}`, 'Content-Type':'application/json'},
+    headers:{Authorization:`Bearer ${sk_live_e2b31fe28d46c220f6059404e44dd875feb854d3}`, 'Content-Type':'application/json'},
     body: JSON.stringify(body)
   });
   const data = await response.json();
