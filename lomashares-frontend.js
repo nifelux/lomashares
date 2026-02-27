fcc8502ebf4020ecf7088feadbe396345c15cc23
nifelux <globalpaysure@gmail.com>
2026-02-27 07:30:54 +0100

    Update lomashares-frontend.js

--- lomashares-frontend.js
+++ lomashares-frontend.js
@@ -6,8 +6,8 @@
 /* ------------------------------
    Supabase Setup
 ------------------------------ */
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://lpnnqxalmihxgszoifpa.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwbm5xeGFsbWloeGdzem9pZnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzM1ODEsImV4cCI6MjA4NzMwOTU4MX0.1hLW5gizjcPTKyfzx_XD9dxqegtXVQroNCclX1AaqZw";
 const supabase = supabase.createClient
   ? supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
   : supabase;
